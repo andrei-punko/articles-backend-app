cdb41a52f9d037ce4f951bcea665ba9c85e72f28
Andrei Punko <Andrei_Punko@epam.com>
2019-10-17 12:21:36 +0300

    Add autogenerated Spring Boot project skeleton

---
+++ src/main/java/by/andd3dfx/articlesbackendapp/ArticlesBackendAppApplication.java
@@ -0,0 +1,13 @@
+package by.andd3dfx.articlesbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/andd3dfx/articlesbackendapp/ArticlesBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.andd3dfx.articlesbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
