a706b200ca3280721f308971fb26de8b18af6d2f
Andrei Punko <Andrei_Punko@epam.com>
2019-10-17 17:15:22 +0300

    Add DTO classes

---
+++ src/main/java/by/andd3dfx/dto/ArticleDto.java
@@ -0,0 +1,70 @@
+package by.andd3dfx.dto;
+
+import java.util.Date;
+
+public class ArticleDto {
+
+    private Long id;
+    private String title;
+    private String summary;
+    private String text;
+    private AuthorDto author;
+    private Date dateCreated;
+    private Date dateUpdated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public AuthorDto getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDto author) {
+        this.author = author;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(Date dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+}
