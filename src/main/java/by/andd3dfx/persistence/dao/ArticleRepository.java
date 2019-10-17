545a24f03d7a8deefb8cd4171b23f2e23fd94e65
Andrei Punko <Andrei_Punko@epam.com>
2019-10-17 18:10:47 +0300

    Add ArticleRepository

---
+++ src/main/java/by/andd3dfx/persistence/dao/ArticleRepository.java
@@ -0,0 +1,10 @@
+package by.andd3dfx.persistence.dao;
+
+import by.andd3dfx.persistence.entities.Article;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+
+}
