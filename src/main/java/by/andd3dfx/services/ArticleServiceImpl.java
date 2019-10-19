a305e9ebe4add21e42b640c301d801a8440bd07a
Andrei Punko <andd3dfx@gmail.com>
2019-10-19 12:51:52 +0300

    Add @Transactional

--- src/main/java/by/andd3dfx/services/ArticleServiceImpl.java
+++ src/main/java/by/andd3dfx/services/ArticleServiceImpl.java
@@ -12,8 +12,10 @@ import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ArticleServiceImpl implements ArticleService {
 
     private final ArticleRepository articleRepository;
