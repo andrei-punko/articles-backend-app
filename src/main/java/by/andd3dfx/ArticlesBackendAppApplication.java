1b22074272352979d3eccd53d8505eac52bd05b1
Andrei Punko <andd3dfx@gmail.com>
2021-09-13 16:33:47 +0300

    Add StartupHelper

--- src/main/java/by/andd3dfx/ArticlesBackendAppApplication.java
+++ src/main/java/by/andd3dfx/ArticlesBackendAppApplication.java
@@ -1,13 +1,17 @@
 package by.andd3dfx;
 
+import by.andd3dfx.util.StartupHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ArticlesBackendAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ArticlesBackendAppApplication.class, args);
+        Environment env = new SpringApplication(ArticlesBackendAppApplication.class)
+                .run(args)
+                .getEnvironment();
+        StartupHelper.logApplicationStartup(env);
     }
-
 }
