4a669edfeb659ee38b351def23befb5778a21607
Andrei Punko <andd3dfx@gmail.com>
2023-04-20 00:52:38 +0300

    Fix issue #17 with web-sockets functionality
    According to: https://stackoverflow.com/questions/66060750/cors-error-when-using-corsfilter-and-spring-security

--- src/main/java/by/andd3dfx/configs/WebSocketConfig.java
+++ src/main/java/by/andd3dfx/configs/WebSocketConfig.java
@@ -31,7 +31,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-client")
-            .setAllowedOrigins("*")
+            .setAllowedOriginPatterns("*")
             .withSockJS();
     }
 
