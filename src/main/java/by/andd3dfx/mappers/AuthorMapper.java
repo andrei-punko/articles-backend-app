9034aa99a160db8ea7862b816a6783f29a443cea
Andrei Punko <andd3dfx@gmail.com>
2024-06-08 14:45:02 +0300

    Avoid warnings about unmapped target

--- src/main/java/by/andd3dfx/mappers/AuthorMapper.java
+++ src/main/java/by/andd3dfx/mappers/AuthorMapper.java
@@ -5,8 +5,11 @@ import by.andd3dfx.persistence.entities.Author;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface AuthorMapper {
 
     AuthorDto toAuthorDto(Author author);
